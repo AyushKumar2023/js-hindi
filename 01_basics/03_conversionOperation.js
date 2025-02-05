d3d9ac04c9b1c9df3f51ff110dba25b93b695726
Ayush Kumar <118875297+AyushKumar2023@users.noreply.github.com>
2025-02-05 18:34:57 +0000

    opertions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,5 @@
+//  ************************ CONVERSION ********************************
+
 let score="23"
 console.log(typeof score);
 console.log(typeof(score));
@@ -26,4 +28,32 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// *********************************** OPERATIONS **********************************
+
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+let str1="Hello"
+let str2=" Ayush"
+let str3=str1+str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+ture);
+console.log(+"");
+
+// Link to study 
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
 
