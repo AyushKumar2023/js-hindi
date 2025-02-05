b3844a26fdb4f38ee9282aff0efa440e8d113fc3
Ayush Kumar <118875297+AyushKumar2023@users.noreply.github.com>
2025-02-05 17:56:20 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello")  // we are using nodejs, not browser
+
+let name="Ayush"
+let age=28
+let isLoggedIn=false
+
+//number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false 
+//null => standalone value
+// undefined
+//symbol => unique
+
+
+
+
+//object
+
+
+console.log(typeof "ayush"); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
