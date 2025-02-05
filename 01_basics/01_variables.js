e7705e5e78d2aaab3fd0744dee0b6c5c6af56c7d
Ayush Kumar <118875297+AyushKumar2023@users.noreply.github.com>
2025-02-05 17:57:41 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=144553
+let accountEmail="ayush@gmail.com"
+var accountPassword="12345"
+accountCity="Bhagalpur"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
