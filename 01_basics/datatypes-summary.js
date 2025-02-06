8b146f06db2c8d000587edf9dc3ef35fb1f74be8
Ayush Kumar <118875297+AyushKumar2023@users.noreply.github.com>
2025-02-06 01:20:34 +0000

    stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,4 +1,4 @@
-// *** Primitive data types
+// *** Primitive data types  (call by value)
 
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
@@ -16,7 +16,7 @@ const anotherId=Symbol("123")
 console.log(id===anotherId);
 
 
-// *** Reference type / Non-Primitive data types
+// *** Reference type / Non-Primitive data types   (call by reference)
 
 // Array, Objects, Functions
 // all has return types of FUNCTION
@@ -36,3 +36,35 @@ console.log(typeof myFunction);
 
 // link to study 
 // https://262.ecma-internatiohal.org/5.1/#sec-11.4.3
+
+
+
+//   ***************************** Memory*******************************
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+// goes in stack 
+
+let myYoutubeName="AyushAmber"
+
+let anothername=myYoutubeName
+anothername="chaiaurcode"
+
+console.log(anothername);
+console.log(myYoutubeName);
+
+// goes in heap
+
+let userOne={
+    name: "XYZ",
+    upi: "ayz@apl"
+}
+
+let userTwo =userOne;
+userTwo.name="Ayush"
+
+console.log(userOne.name);
+console.log(userTwo.name);
+
+
+
