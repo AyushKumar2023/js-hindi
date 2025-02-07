a969a9d5a004d114620e0612960a4fc7b76d62b3
Ayush Kumar <118875297+AyushKumar2023@users.noreply.github.com>
2025-02-07 02:50:06 +0000

    if

---
+++ 04_control_flow/one.js
@@ -0,0 +1,37 @@
+// if
+
+// const score=200
+
+// if(score>100){
+//     let power="fly"
+//     console.log(`user power : ${power}`);
+    
+// }
+
+// console.log(`user power : ${power}`);
+
+// const balance=1000
+
+// if(balance>500) console.log("test");  // do not write like this
+
+// if(balance<500){
+//     console.log("less than 500");
+    
+// }else if(balance<750){
+//     console.log("less than 750");
+    
+// }else if(balance<900){
+//     console.log("less than 900");
+    
+// }else{
+//     console.log("less than 1200");
+// }
+
+
+const userLoggedIn=true
+const debitCard=true
+
+if(userLoggedIn && debitCard){
+    console.log("allow to purchase");
+    
+}
