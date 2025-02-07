2b5ccef067e4b343b39915497dc89d358fe69707
Ayush Kumar <118875297+AyushKumar2023@users.noreply.github.com>
2025-02-07 17:06:36 +0000

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,43 @@
+// high order array loops
+
+// ["", "", ""] 
+// [{}, {}, {}]
+
+
+// for of loop
+const arr=[1,2,3,4,5]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings="hello world"
+
+for (const greet of greetings) {
+    // console.log(`each char is ${greet}`);
+}
+
+
+// Maps
+
+const map=new Map()
+map.set("IN", "India")
+map.set("USA", "United States of America")
+map.set("FR", "France")
+
+// console.log(map);
+
+// for of loop on map
+for (const [key,value] of map) {
+    // console.log(key,":-",value);
+}
+
+const myobj={
+    name: "Ayush",
+    age: 23,
+    country: "India"
+}
+
+// for (const ele of myobj) {
+//     console.log(ele);          // for of loop is not able to iterate on object in javascript
+// }
